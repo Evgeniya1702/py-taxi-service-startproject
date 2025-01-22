8972bd0c5bbf5fd43e4187442257ba22a7548445
Evgeniya <didovichevgeniya@gmail.com>
2025-01-22 21:03:36 +0200

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -3,22 +3,22 @@ from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=120, unique=True)
-    country = models.CharField(max_length=120, unique=True)
+    name = models.CharField(max_length=250, unique=True)
+    country = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return self.name
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=120, unique=True)
+    license_number = models.CharField(max_length=250, unique=True)
 
     def __str__(self):
         return f"{self.username} ({self.license_number})"
 
 class Car(models.Model):
-    model = models.CharField(max_length=120)
+    model = models.CharField(max_length=250)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, related_name="cars")
-    drivers = models.ManyToManyField("Driver", max_length=120, related_name="cars")
+    drivers = models.ManyToManyField("Driver", max_length=250, related_name="cars")
 
     def __str__(self):
         return f"{self.model} ({self.manufacturer})"
